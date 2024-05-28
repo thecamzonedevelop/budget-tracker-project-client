0a64cf820e2331db1d88b3741feed917ea486e4b
VANN YA <van.soriya.1221@rupp.edu.kh>
2024-05-28 17:00:40 +0700

    chore: Add Element Plus library for UI components and update dev server configuration

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    <el-button>I am ElButton</el-button>
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
@@ -13,6 +14,22 @@ export default {
   name: "HomeView",
   components: {
     HelloWorld,
+  },
+  data() {
+    return {
+      posts: [],
+    };
+  },
+  mounted() {
+   
+  },
+  async created() {
+    const response = await fetch('/api/posts');
+    const data = await response.json();
+    this.posts = data;
+  },
+  methods: {
+      
   },
 };
 </script>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,20 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
+  publicPath: '/kh/',
+  outputDir: 'dist',
   transpileDependencies: true,
+  devServer: {
+    open: true,
+    proxy: {
+      '/api': {
+        target: 'https://jsonplaceholder.typicode.com',
+        changeOrigin: true,
+        ws: true,
+        pathRewrite: {
+          '^/api': ''
+        }
+      }
+    },
+
+  }
 });
